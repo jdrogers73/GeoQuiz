f6bf9dda5359635911112ee39af9ca1982ac1ca8
jdrogers73 <james.rogers.web@gmail.com>
2015-05-08 22:25:42 -0400

    Completed Chapter 1

--- app/src/main/java/uncp/edu/geoquiz/QuizActivity.java
+++ app/src/main/java/uncp/edu/geoquiz/QuizActivity.java
@@ -4,14 +4,39 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends Activity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        //Relate the layout to the view.
+        //Link the layout file to the class file
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener()
+        {
+            //When the button is clicked, execute the code in onClick()
+            public void onClick(View v)
+            {
+                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT);
+            }
+        });
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT);
+            }
+        });
     }
 
 
