d2080a6decc3d95eaa79f9807a45083aa6d84f06
jdrogers73 <james.rogers.web@gmail.com>
2015-05-27 22:32:09 -0400

    Completed Chapter 2 Guided Content
    Completed all book-lead development. Have not done challenges.

--- app/src/main/java/uncp/edu/geoquiz/QuizActivity.java
+++ app/src/main/java/uncp/edu/geoquiz/QuizActivity.java
@@ -7,16 +7,55 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.widget.TextView;
 
 public class QuizActivity extends Activity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+    private int mCurrentIndex = 0;
+
+    private TrueFalse[] mQuestionBank = new TrueFalse[]
+            {
+                    new TrueFalse(R.string.question_oceans, true),
+                    new TrueFalse(R.string.question_raleigh, true),
+                    new TrueFalse(R.string.question_car, false)
+            };
+
+    private void updateQuestion()
+    {
+        int question = mQuestionBank [mCurrentIndex].getQuestion();
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue)
+    {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+        int messageResId = 0;
+
+        if (userPressedTrue == answerIsTrue)
+        {
+            messageResId = R.string.correct_toast;
+        }
+        else
+        {
+            messageResId = R.string.incorrect_toast;
+        }
+
+        Toast.makeText(this,messageResId,Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        updateQuestion();
+        //int question = mQuestionBank[mCurrentIndex].getQuestion();
+        //mQuestionTextView.setText(question);
 
         //Relate the layout to the view.
         //Link the layout file to the class file
@@ -26,19 +65,34 @@ public class QuizActivity extends Activity {
             //When the button is clicked, execute the code in onClick()
             public void onClick(View v)
             {
-                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT);
+                //Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
             }
         });
+
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View v)
             {
-                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT);
+                //Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
+            }
+        });
+
+        //add Next Button to the screen
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                //int question = mQuestionBank[mCurrentIndex].getQuestion();
+                //mQuestionTextView.setText(question);
+                updateQuestion();
             }
         });
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
